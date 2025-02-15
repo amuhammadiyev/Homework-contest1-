f91c50021e70c37be9ab866760c8a4d96018b544
amuhammadiyev <a.muhammadiyev@newuu.uz>
2025-02-15 19:54:36 +0500

    Update task1.py

--- task1.py
+++ task1.py
@@ -2,6 +2,3 @@
 def kwargsAcceptFun(**alex):
     for name, value in alex.items():
         print(f"{name}: {value}")
-
-
-# kwargsAcceptFun(name="Asilbek", age=19, city="Tashkent", occupation="Student")  #well I used this raw just to see if it is working
