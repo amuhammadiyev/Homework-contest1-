263a3b5b9d25acf85e1d5d2ecff33867eb99b82c
amuhammadiyev <a.muhammadiyev@newuu.uz>
2025-02-15 20:53:04 +0500

    Update main.py

--- main.py
+++ main.py
@@ -2,4 +2,12 @@ from task1 import kwargsAcceptFun
 kwargsAcceptFun(name="Asilbek", age=19, city="Tashkent", occupation="Student")
 print()
 
-from task2 import kwargsAcceptFun
+
+
+from task2 import typeBasedTransformer
+a= typeBasedTransformer(name="Asilbek",age=19,flag=True,data_list=[1, 2, 3],data_tuple=(7, 9, 11),data_dict={"a": 1, "b": 2})
+print(a)
+print()
+
+from task3 import 
+
