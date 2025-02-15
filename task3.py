9ceb6e1d9b433775b18c388cc62b2948c14c67bc
amuhammadiyev <a.muhammadiyev@newuu.uz>
2025-02-15 21:08:02 +0500

    Update task3.py

--- task3.py
+++ task3.py
@@ -1,1 +1,12 @@
-def decorator_1
+import time
+import random
+
+def decorator_1(func):
+    def task3(*args, **kwargs):
+        starting = time.time()
+        result = func(*args, **kwargs)
+        ending = time.time()
+        executionn = ending - starting
+        print(f"{func.__name__} call executed in {executionn:.4f} sec")
+        return result
+    return task3
